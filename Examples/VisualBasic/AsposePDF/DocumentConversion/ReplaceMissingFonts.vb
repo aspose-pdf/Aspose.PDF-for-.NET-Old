b684c0be2e114f3774b8fc2fdac7d531f92e9df4
Naeem Akram <naeem.akram@aspose.com>
2016-08-04 13:26:10 +0500

    Add example Replace Missing Fonts for Document Conversion

---
+++ Examples/VisualBasic/AsposePDF/DocumentConversion/ReplaceMissingFonts.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Imports System
+Imports Aspose.Pdf
+Imports Aspose.Pdf.Text
+
+Namespace AsposePDF.DocumentConversion
+    Public Class ReplaceMissingFonts
+        Public Shared Sub Run()
+            ' ExStart:ReplaceMissingFonts
+            ' The path to the documents directory.
+            Dim dataDir As String = RunExamples.GetDataDir_AsposePdf_DocumentConversion()
+
+            Dim originalFont As Aspose.Pdf.Text.Font = Nothing
+            Try
+                originalFont = FontRepository.FindFont("AgencyFB")
+            Catch generatedExceptionName As Exception
+                'Font is missing on destination machine
+                FontRepository.Substitutions.Add(New SimpleFontSubstitution("AgencyFB", "Arial"))
+            End Try
+            Dim fileNew = New FileInfo(dataDir & Convert.ToString("newfile_out_.pdf"))
+            Dim pdf = New Document(dataDir & Convert.ToString("input.pdf"))
+            pdf.Convert(dataDir & Convert.ToString("log.xml"), PdfFormat.PDF_A_1B, ConvertErrorAction.Delete)
+            pdf.Save(fileNew.FullName)
+            ' ExEnd:ReplaceMissingFonts
+        End Sub
+    End Class
+End Namespace
