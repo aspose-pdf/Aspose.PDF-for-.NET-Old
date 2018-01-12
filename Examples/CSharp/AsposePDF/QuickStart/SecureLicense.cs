d4349eca55777125c2da0c710f152ed7b67a00c5
Asad Ali <asad.ali@aspose.com>
2018-01-12 15:07:44 +0500

    Included DotNetZip from NuGet

--- Examples/CSharp/AsposePDF/QuickStart/SecureLicense.cs
+++ Examples/CSharp/AsposePDF/QuickStart/SecureLicense.cs
@@ -10,16 +10,13 @@ namespace Aspose.Pdf.Examples.CSharp.AsposePDF.QuickStart
             // ExStart:1
             using (Stream zip = new SecureLicense().GetType().Assembly.GetManifestResourceStream("Aspose.Total.lic.zip"))
             {
-                // Download DotNetZip utility from (http://dotnetzip.codeplex.com/) and uncomment following code 
-                // after referencing Ionic.Zip.Reduced.dll from downloaded package.
-
-                //using (ZipFile zf = ZipFile.Read(zip))
-                //{
-                //    MemoryStream ms = new MemoryStream();
-                //    ZipEntry e = zf["Aspose.Total.lic"];
-                //    e.ExtractWithPassword(ms, "test");
-                //    ms.Position = 0;
-                //}
+                using (ZipFile zf = ZipFile.Read(zip))
+                {
+                    MemoryStream ms = new MemoryStream();
+                    ZipEntry e = zf["Aspose.Total.lic"];
+                    e.ExtractWithPassword(ms, "test");
+                    ms.Position = 0;
+                }
             }
             // ExEnd:1
         }
